acda1d0aa9362277ecefb45593bf1e00ad7176c7
Malegr0 <schoening.andreas@gmx.de>
2021-09-14 09:50:23 +0200

    creation of window added

--- GDP2021/Window.cpp
+++ GDP2021/Window.cpp
@@ -1,5 +1,15 @@
 #include "Window.h"
 
+LRESULT CALLBACK WndProc(
+    HWND hWnd,  // handle to window isntance this message is sent to
+    UINT msg,   // message number
+    WPARAM wParam,  // important message property (main message property)(e.g. which key got pressed)
+    LPARAM lParam   // additional message properties (e.g. which key got pressed -> repeat count, value...)
+)
+{
+    return DefWindowProc(hWnd, msg, wParam, lParam);    //everytime there happens something (moving the mouse, window...
+}
+
 INT Window::init(HINSTANCE hInstance, INT width, INT height, INT nCMDShow)
 {
     // 1. describe the windwo class
@@ -11,12 +21,28 @@ INT Window::init(HINSTANCE hInstance, INT width, INT height, INT nCMDShow)
     wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;  // if window is changed horizontal or vertical then we force a redraw of the whole project, (pipe is a bit-or-gate)
                                                     // not visual style, but how windows is using this window or simple window instance properties
     wc.lpszClassName = TEXT("Direct3D 9");
+    wc.lpfnWndProc = WndProc;   //long pointer function
+                                // pointer to communication function (short Window Procedure)
 
     // 2. register window class
+    if (!RegisterClass(&wc)) return 10;
 
     // 3. calculate window size (optional)
 
     // 4. create window instance
+    _hWnd = CreateWindow(
+        wc.lpszClassName,   // window class name a window should be created from
+        wc.lpszClassName,   // window title
+        WS_OVERLAPPEDWINDOW,    // window visual style
+        100, 100,   // window position (left-top corner)
+        width, height,  // window size
+        nullptr,    // handle to parent window
+        nullptr,    // handle to menu object
+        hInstance,
+        nullptr     // optional parameters
+    );
+
+    if (!_hWnd) return 15;
 
     // 5. show window
 
